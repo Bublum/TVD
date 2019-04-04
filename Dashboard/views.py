ed9813e2d9724158a9033a22df8bb6cae277cdb6
rahul <rshahshah2890@gmail.com>
2019-04-04 22:19:20 +0530

    all input videos

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -8,7 +8,9 @@ def homepage(request):
     if request.method == 'GET':
         all_input = Input.objects.filter(is_active=True)
 
-        input_json = {}
+        # input_json = {}
+        #
+        # for each_input in all_input:
+        #     input_json[each_input.name] = each_input.file.path
 
-        for each_input in all_input:
-            input_json[each_input.name] = each_input.file.path
+        return render(request, '',{'all_input':all_input})
