4c9ef240c5158d4855716cf820a2c1077f655058
rahul <rshahshah2890@gmail.com>
2019-04-04 22:03:59 +0530

    all input videos

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from Detection.models import Input
+
+
+def homepage(request):
+    if request.method == 'GET':
+        all_input = Input.objects.filter(is_active=True)
+
+        input_json = {}
+
+        for each_input in all_input:
+            input_json[each_input.name] = each_input.file.path
