7e8cf798f2ff01ad0accb4b4db577d19f8081a32
anishsatalkar <anishsatalkar@gmail.com>
2019-05-01 22:01:18 +0530

    CELERY TASKS.PY

--- Dashboard/tasks.py
+++ Dashboard/tasks.py
@@ -1,7 +1,12 @@
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+app = Celery('tasks', broker='pyamqp://guest@localhost//', backend='rpc://')
 
 @app.task
 def add(x, y):
+    for i in range(200000000):
+        x +=y
+        y+=1
     return x + y
+
+add.delay(4,4)
