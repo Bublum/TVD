96ca5a70d5ce5d84623ad313b0b646f3528defa6
rahul <rshahshah2890@gmail.com>
2019-05-01 22:03:01 +0530

    major changes

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 
 
-# Create your models here.
+def vehicle_detection_directory(instance, filename):
+    return 'Media/Vehicle/{0}/Research/{1}'.format(instance.faculty.faculty_code, filename)
 
-class Detection(models.Model):
+
+class Model(models.Model):
     model_type = models.CharField(max_length=100)
-    model_path = models.FileField(max_length=1000)
-    label_path = models.FileField(max_length=1000)
+    model = models.FileField(max_length=1000)
+    label = models.FileField(max_length=1000)
     model_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
 
 
-class VehicleDetection(models.Model):
+class VehicleTypeMaster(models.Model):
+    vehicle_type = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+
+class VehicleMonitor(models.Model):
     number = models.CharField(max_length=15, null=True, blank=True)
     mobile = models.PositiveIntegerField(default='8554951545')
     address = models.CharField(max_length=250, default='test')
-    type = models.CharField(max_length=100)
+    vehicel_type = models.ForeignKey(VehicleTypeMaster, on_delete=models.CASCADE)
     image = models.FileField(max_length=1000)
     is_done = models.BooleanField(default=False)
 
@@ -33,7 +40,7 @@ class Input(models.Model):
     file_type = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
     is_processed = models.BooleanField(default=False)
-
+    location = models.ForeignKey(Camera, on_delete=models.CASCADE)
 
 class ViolationMaster(models.Model):
     name = models.CharField(max_length=100)
@@ -47,7 +54,7 @@ class ViolationMaster(models.Model):
 #     violation = models.ForeignKey(ViolationMaster, on_delete=models.CASCADE)
 
 class VehicleViolation(models.Model):
-    vehicle = models.ForeignKey(VehicleDetection, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(VehicleMonitor, on_delete=models.CASCADE)
     camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
     violation = models.ForeignKey(ViolationMaster, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
@@ -60,3 +67,9 @@ class Config(models.Model):
     fps = models.DecimalField(max_digits=7, decimal_places=3)
     min_threshold = models.DecimalField(max_digits=7, decimal_places=3)
     max_predict_class = models.PositiveIntegerField()
+
+
+class VehicleDetection(models.Model):
+    image = models.ImageField()
+    vehicle_type = models.ForeignKey(VehicleTypeMaster, on_delete=models.CASCADE)
+    location = models.ForeignKey(Camera, on_delete=models.CASCADE)
--- Dashboard/tasks.py
+++ Dashboard/tasks.py
@@ -1,10 +1,18 @@
+import copy
+
 import cv2
+from PIL import Image
 from celery import Celery, shared_task
 
 # from Dashboard.models import Input, Detection
 import tensorflow as tf
+import numpy as np
 
-from object_detection.util import label_map_util
+from Dashboard.config import maximum_classes_to_detect, min_score_thresh, categories_to_detect
+from Dashboard.models import Model, Input, VehicleTypeMaster, VehicleDetection
+from object_detection.functions import count_frames_manual
+from object_detection.ops import reframe_box_masks_to_image_masks
+from object_detection.utils import label_map_util
 
 app = Celery('tasks', backend='rpc://', broker='amqp://test:test@127.0.0.1//')
 
@@ -50,4 +58,74 @@ def vehicle_detection(x, y):
                                                                     use_display_name=True)
         category_index = label_map_util.create_category_index(categories)
 
-    return x + y
+        with detection_graph.as_default():
+            with tf.Session() as sess:
+                for each_frame in count_frames_manual(cap):
+                    rev, image_np = cap.read()
+                    # the array based representation of the image will be used later in order to prepare the
+                    # result image with boxes and labels on it.
+                    # Expand dimensions since the Prediction expects images to have shape: [1, None, None, 3]
+                    image_np_expanded = np.expand_dims(image_np, axis=0)
+                    # Actual detection.
+                    ops = tf.get_default_graph().get_operations()
+                    all_tensor_names = {output.name for op in ops for output in op.outputs}
+                    tensor_dict = {}
+                    for key in [
+                        'num_detections', 'detection_boxes', 'detection_scores',
+                        'detection_classes', 'detection_masks'
+                    ]:
+                        tensor_name = key + ':0'
+                        if tensor_name in all_tensor_names:
+                            tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                                tensor_name)
+                    if 'detection_masks' in tensor_dict:
+                        # The following processing is only for single image
+                        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+                        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+                        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+                        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+                        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+                        detection_masks_reframed = reframe_box_masks_to_image_masks(
+                            detection_masks, detection_boxes, image_np.shape[0], image_np.shape[1])
+                        detection_masks_reframed = tf.cast(
+                            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                        # Follow the convention by adding back the batch dimension
+                        tensor_dict['detection_masks'] = tf.expand_dims(
+                            detection_masks_reframed, 0)
+                    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+                    # Run inference
+                    output_dict = sess.run(tensor_dict,
+                                           feed_dict={image_tensor: np.expand_dims(image_np, 0)})
+
+                    # all outputs are float32 numpy arrays, so convert types as appropriate
+                    output_dict['num_detections'] = int(output_dict['num_detections'][0])
+                    output_dict['detection_classes'] = output_dict[
+                        'detection_classes'][0].astype(np.uint8)
+                    output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+                    output_dict['detection_scores'] = output_dict['detection_scores'][0]
+                    if 'detection_masks' in output_dict:
+                        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+
+                    image_np_copy = copy.deepcopy(image_np)
+
+                    num_detected_vehicles = output_dict['num_detections']
+
+                    for i in range(num_detected_vehicles):
+
+                        if output_dict['detection_scores'][i] >= min_score_thresh:
+                            class_name = category_index[output_dict['detection_classes'][i]]['name']
+                            if class_name in categories_to_detect:
+                                coord = output_dict['detection_boxes'][i]
+                                y1, x1, y2, x2 = coord[0], coord[1], coord[2], coord[3]
+                                y1 = int(y1 * image_np.shape[0])
+                                y2 = int(y2 * image_np.shape[0])
+                                x1 = int(x1 * image_np.shape[1])
+                                x2 = int(x2 * image_np.shape[1])
+                                cropped_img = image_np[y1:y2, x1:x2]
+                                class_obj = VehicleTypeMaster.objects.get(vehicle_type=class_name)
+                                rescaled = np.uint8(cropped_img)
+                                im = Image.fromarray(rescaled)
+                                VehicleDetection.objects.create(vehicle_type=class_obj, image=im)
+    return True
